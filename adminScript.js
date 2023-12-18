137a553fac43b11bb85be4e8960b58745600acee
Enzo-El <101322841+Enzo-El@users.noreply.github.com>
2023-12-18 23:25:48 +0800

    User and Admin Page (temporary admin page)
    Adding "userPage.html"
    Adding "userStyle.css"
    Adding "userScript.js"
    Adding "adminPage.html" - Temp
    Adding "adminStyle.css" - temp
    Adding "adminScript.js" - temp

---
+++ adminScript.js
@@ -0,0 +1,26 @@
+//  Student Name: Iverson Craig Gonzales Guno
+//  Student Name: Roy Icaranom Manaog
+//  Student Name: Lorenzo Laurel Reyes
+//  Class Batch: WD92P (ZetoRo)
+//  MP2 Group No.: Group 3 KonohaGakure
+//  Schedule: 7:00pm to 10:00pm
+//  Instructor: Ma'am Jamie Delina
+//  Coordinator: Sir Ben De Leon
+
+// SIDEBAR TOGGLE
+let sidebarOpen = false;
+const sidebar = document.getElementById('sidebar');
+
+function openSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add('sidebar-responsive');
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove('sidebar-responsive');
+    sidebarOpen = false;
+  }
+}
