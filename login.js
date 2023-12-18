b4ef0d9b90b0ed087f4a62fb8406c74e47d64373
Kirukushikun <iversoncraigg@gmail.com>
2023-12-19 01:59:55 +0800

    modified:   logIn.html 	modified:   login.js 	modified:   signUp.css 	modified:   signUpAdmin.html 	modified:   signUpUser.html 	modified:   signup.js

--- login.js
+++ login.js
@@ -1,65 +1,77 @@
-let userRes = document.getElementById('userRespo');
-let passRes = document.getElementById('passRespo');
+var storedUser = localStorage.getItem("username");
+var storedPass = localStorage.getItem("password");
 
-function scanUser(){
-    for(let i = 0; i < username.length; i++){
-        let num1 = username[i];
-        let num2 = password[i];
-        verify(num1, num2);
+var username = JSON.parse(storedUser);
+var password = JSON.parse(storedPass);
+
+let userRes = document.getElementById("userRespo");
+let passRes = document.getElementById("passRespo");
+
+async function scanUser() {
+  let accInput = document.getElementById("email").value;
+  let passInput = document.getElementById("password").value;
+
+  for (let i = 0; i < username.length; i++) {
+    num1 = username[i];
+    num2 = password[i];
+    if (num1 === accInput || num2 === passInput) {
+      verify(num1, num2);
+      break;
+    } else {
+      userRes.innerText = "Username incorrect";
+      passRes.innerText = "Password incorrect";
     }
+  }
 }
 
-function verify(USERNAME, PASSWORD){
-    let accInput = document.getElementById('email').value;
-    let passInput = document.getElementById('password').value;
+function verify(USERNAME, PASSWORD) {
+  let accInput = document.getElementById("email").value;
+  let passInput = document.getElementById("password").value;
 
-    userRes.innerText = '';
-    passRes.innerText = '';
+  userRes.innerText = "";
+  passRes.innerText = "";
 
-    if (USERNAME === accInput && PASSWORD === passInput){
-        window.location.href = 'index.html';
-    }else if(USERNAME !== accInput && PASSWORD !== passInput){
-        userRes.innerText = 'Username incorrect';
-        passRes.innerText = 'Password incorrect';       
-    }else if(USERNAME === accInput && PASSWORD !== passInput){
-        passRes.innerText = 'Password incorrect';       
-    }else{
-        userRes.innerText = 'Username incorrect';
-    }
+  if (USERNAME === accInput && PASSWORD === passInput) {
+    window.location.href = "index.html";
+  } else if (USERNAME === accInput && PASSWORD !== passInput) {
+    passRes.innerText = "Password incorrect";
+  } else if (USERNAME !== accInput && PASSWORD === passInput) {
+    userRes.innerText = "Username incorrect";
+  }
 }
 
-function access(){
-    let accInput = document.getElementById('email').value;
-    let passInput = document.getElementById('password').value;
+function access() {
+  let accInput = document.getElementById("email").value;
+  let passInput = document.getElementById("password").value;
 
-    userRes.innerText = '';
-    passRes.innerText = '';
+  userRes.innerText = "";
+  passRes.innerText = "";
 
-    if(accInput == "" && passInput == ""){
-        userRes.innerText = 'Please enter Username';
-        passRes.innerText = 'Please enter Password';
-    }else if(accInput == "" && passInput !== ""){
-        userRes.innerText = 'Please enter Username';
-    }else if(accInput !== "" && passInput == ""){
-        passRes.innerText = 'Please enter Password';
-    }else{
-        scanUser();
-    }
+  if (accInput == "" && passInput == "") {
+    userRes.innerText = "Please enter Username";
+    passRes.innerText = "Please enter Password";
+  } else if (accInput == "" && passInput !== "") {
+    userRes.innerText = "Please enter Username";
+  } else if (accInput !== "" && passInput == "") {
+    passRes.innerText = "Please enter Password";
+  } else {
+    scanUser();
+  }
 }
 
 // -------------------------------------------
-function reveal(){
-    let password = document.getElementById('password');
-    let icon = document.getElementById('icon');
-    password.type = 'text';
-    icon.classList.remove('bi-eye-slash-fill');
-    icon.classList.add('bi-eye-fill');
+function reveal() {
+  let password = document.getElementById("password");
+  let icon = document.getElementById("icon");
+  password.type = "text";
+  icon.classList.remove("bi-eye-slash-fill");
+  icon.classList.add("bi-eye-fill");
 }
 
-function unreveal(){
-    let password = document.getElementById('password');
-    password.type = 'password';
-    icon.classList.remove('bi-eye-fill');
-    icon.classList.add('bi-eye-slash-fill');
+function unreveal() {
+  let password = document.getElementById("password");
+  password.type = "password";
+  icon.classList.remove("bi-eye-fill");
+  icon.classList.add("bi-eye-slash-fill");
 }
 // -------------------------------------------
