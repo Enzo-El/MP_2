70ac3988720ee762d864a5ef63a47e08465b0e83
Enzo-El <101322841+Enzo-El@users.noreply.github.com>
2024-01-03 18:52:19 +0800

    Update userScript.js

--- userScript.js
+++ userScript.js
@@ -29,6 +29,35 @@ function closeSidebar() {
   }
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  updateIndexTable();
+});
+
+function updateIndexTable() {
+  const indexTableBody = document.getElementById('indexTableBody');
+  indexTableBody.innerHTML = '';
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const userData = JSON.parse(localStorage.getItem(key));
+    
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${userData.caseNo}</td>
+      <td>${userData.completeName}</td>
+      <td>${userData.residence}</td>
+      <td>${userData.email}</td>
+      <td>${userData.phone}</td>
+      <td>${userData.blotter}</td>
+      <td>${userData.narrative}</td>
+      <td>${userData.incidentDate}</td>
+      <td>${userData.incidentPicture}</td>
+      <td>${userData.status}</td>
+    `;
+    indexTableBody.appendChild(row);
+  }
+}
+
 function capitalizeFirstLetter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
